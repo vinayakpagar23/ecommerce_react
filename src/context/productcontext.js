9dcacc3cca60fd7ba49793ae0cb1e7867f9f3ace
vinayakpagar23 <vinayakpagar1999@gmail.com>
2022-12-29 21:07:07 +0530

    fetch api done

--- src/context/productcontext.js
+++ src/context/productcontext.js
@@ -1,8 +1,22 @@
+import { useEffect } from "react";
 import  {createContext,useContext} from "react";
+import axios from "axios";
 
 const AppContext=createContext();
 
+const API ="https://api.pujakaitem.com/api/products";
+
 const AppProvider=({children})=>{ 
+    const getProducts= async(url)=>{
+      const res=await axios.get(url);  
+      console.log(res);
+      const product =await res.data;
+      console.log(product);
+
+    }
+    useEffect(()=>{
+          getProducts(API);
+    },[])
     return <AppContext.Provider value={{myName:"Vinayak"}}>
         {children}
     </AppContext.Provider>
