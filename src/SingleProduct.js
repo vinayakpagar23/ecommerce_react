b82d68f1953378181c0274642be80ffe1b3bb60c
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-01-11 23:18:23 +0530

    single product page

--- src/SingleProduct.js
+++ src/SingleProduct.js
@@ -1,6 +1,33 @@
+import { useEffect } from "react";
 import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import { useProductContext } from "./context/productcontext";
+
+const API = "https://api.pujakaitem.com/api/products";
+
 const SingleProduct = () => {
-  return <Wrapper></Wrapper>;
+  const { getSingleProduct, isSingleLoading, singleProduct } =
+    useProductContext();
+
+  const { id } = useParams();
+
+  const {
+    id: alias,
+    name,
+    company,
+    price,
+    description,
+    category,
+    stock,
+    stars,
+    reviews,
+  } = singleProduct;
+
+  useEffect(() => {
+    getSingleProduct(`${API}?id=${id}`);
+  }, []);
+
+  return <h1>single page {name} </h1>;
 };
 
 const Wrapper = styled.section`
@@ -13,7 +40,6 @@ const Wrapper = styled.section`
     align-items: flex-start;
     justify-content: center;
     gap: 2rem;
-
     .product-data-warranty {
       width: 100%;
       display: flex;
@@ -21,10 +47,8 @@ const Wrapper = styled.section`
       align-items: center;
       border-bottom: 1px solid #ccc;
       margin-bottom: 1rem;
-
       .product-warranty-data {
         text-align: center;
-
         .warranty-icon {
           background-color: rgba(220, 220, 220, 0.5);
           border-radius: 50%;
@@ -38,7 +62,6 @@ const Wrapper = styled.section`
         }
       }
     }
-
     .product-data-price {
       font-weight: bold;
     }
@@ -50,12 +73,10 @@ const Wrapper = styled.section`
       flex-direction: column;
       gap: 1rem;
       font-size: 1.8rem;
-
       span {
         font-weight: bold;
       }
     }
-
     hr {
       max-width: 100%;
       width: 90%;
@@ -64,13 +85,11 @@ const Wrapper = styled.section`
       color: red;
     }
   }
-
   .product-images {
     display: flex;
     justify-content: center;
     align-items: center;
   }
-
   @media (max-width: ${({ theme }) => theme.media.mobile}) {
     padding: 0 2.4rem;
   }
--- src/context/productcontext.js
+++ src/context/productcontext.js
@@ -1,69 +1,61 @@
-import { useEffect } from "react";
-import  {createContext,useContext} from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import axios from "axios";
-import { useReducer } from "react";
 import reducer from "../reducer/ProductReducer";
 
-const AppContext=createContext();
+const AppContext = createContext();
 
-const API ="https://api.pujakaitem.com/api/products";
+const API = "https://api.pujakaitem.com/api/products";
 
-const initialState={
-    isLoading:false,
-    isError:false,
-    products:[],
-    featureProducts:[],
-    isSingleProduct:false,
-    singleProduct:{},
+const initialState = {
+  isLoading: false,
+  isError: false,
+  products: [],
+  featureProducts: [],
+  isSingleLoading: false,
+  singleProduct: {},
+};
 
-}
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-const AppProvider=({children})=>{ 
-
-    const [state,dispatch]=useReducer(reducer,initialState);
-    const getProducts= async(url)=>{
-        dispatch({type:"SET_LOADING"});
-        try{
-            const res=await axios.get(url);  
-            //   console.log(res);
-              const products =await res.data;
-            //   console.log(product);
-            dispatch({type:"SET_API_DATA",payload:products})
-        }
-        catch(error){
-           dispatch({type:"API_ERROR"});
-        }
-     
-
+  const getProducts = async (url) => {
+    dispatch({ type: "SET_LOADING" });
+    try {
+      const res = await axios.get(url);
+      const products = await res.data;
+      dispatch({ type: "SET_API_DATA", payload: products });
+    } catch (error) {
+      dispatch({ type: "API_ERROR" });
     }
+  };
 
-// for single product 
-const getSingleProduct= async(url)=>{
-    dispatch({type:"SET_LOADING"});
-    try{
-        const res=await axios.get(url);  
-        //   console.log(res);
-          const products =await res.data;
-        //   console.log(product);
-        dispatch({type:"SET_API_DATA",payload:products})
+  // my 2nd api call for single product
+
+  const getSingleProduct = async (url) => {
+    dispatch({ type: "SET_SINGLE_LOADING" });
+    try {
+      const res = await axios.get(url);
+      const singleProduct = await res.data;
+      dispatch({ type: "SET_SINGLE_PRODUCT", payload: singleProduct });
+    } catch (error) {
+      dispatch({ type: "SET_SINGLE_ERROR" });
     }
-    catch(error){
-       dispatch({type:"API_ERROR"});
-    }
- 
+  };
 
-}
+  useEffect(() => {
+    getProducts(API);
+  }, []);
 
-    useEffect(()=>{
-          getProducts(API);
-    },[]);
-    return <AppContext.Provider value={{...state}}>
-        {children}
+  return (
+    <AppContext.Provider value={{ ...state, getSingleProduct }}>
+      {children}
     </AppContext.Provider>
+  );
 };
 
 // custom hooks
-const useProductContext=()=>{
-    return useContext(AppContext);
-}
-export {AppProvider,AppContext,useProductContext};
+const useProductContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppProvider, AppContext, useProductContext };
